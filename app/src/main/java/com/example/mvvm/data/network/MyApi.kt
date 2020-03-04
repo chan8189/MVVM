6a25a5861d9fce231b2820d05badc3a1b4a9dbb5
Chandan Choudhary <chaudhary.chandan48@gmail.com>
2020-03-04 07:07:47 +0530

    Login api Implementation Commit

--- app/src/main/java/com/example/mvvm/data/network/MyApi.kt
+++ app/src/main/java/com/example/mvvm/data/network/MyApi.kt
@@ -1,17 +1,22 @@
 package com.example.mvvm.data.network
 
-import android.telecom.Call
+import com.example.mvvm.BuildConfig
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import okhttp3.ResponseBody
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+import java.util.concurrent.TimeUnit
 
 interface MyApi {
 
 
     @FormUrlEncoded
+
     @POST("login")
     fun userLogin(
         @Field("username") userId: String,
@@ -21,12 +26,43 @@ interface MyApi {
     ): retrofit2.Call<ResponseBody>
 
     companion object {
-        operator fun invoke(): MyApi{
+        private const val CONNECTION_TIMEOUT: Long = 30
+        private const val NETWORK_CALL_TIMEOUT: Long = 30
+
+        operator fun invoke(): MyApi {
             return Retrofit.Builder()
                 .baseUrl("https://exlmobility-uat.exlservice.com/OneEXL/")
+                .client(client)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(MyApi::class.java)
         }
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(provideHeaderInterceptor())
+            .addInterceptor(
+                HttpLoggingInterceptor()
+                    .apply {
+                        level = if (BuildConfig.DEBUG)
+                            HttpLoggingInterceptor.Level.BODY
+                        else
+                            HttpLoggingInterceptor.Level.NONE
+                    })
+            .connectTimeout(NETWORK_CALL_TIMEOUT, TimeUnit.SECONDS)
+            .readTimeout(NETWORK_CALL_TIMEOUT, TimeUnit.SECONDS)
+            .writeTimeout(NETWORK_CALL_TIMEOUT, TimeUnit.SECONDS)
+            .build()
+
+        private fun provideHeaderInterceptor(): Interceptor {
+            return Interceptor { chain: Interceptor.Chain ->
+                val accessToken = ""
+                val request = chain.request().newBuilder()
+                    .addHeader("Accept", "application/json")
+                    .addHeader("Authorization", "Bearer $accessToken").build()
+                chain.proceed(request)
+            }
+        }
+
+
     }
 }
--- app/src/main/java/com/example/mvvm/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/example/mvvm/ui/auth/LoginActivity.kt
@@ -1,6 +1,9 @@
 package com.example.mvvm.ui.auth
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.LiveData
@@ -20,9 +23,15 @@ class LoginActivity : AppCompatActivity(), AuthListner {
         super.onCreate(savedInstanceState)
         val binding: ActivityLoginBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_login)
+
         val viewModel = ViewModelProviders.of(this).get(AuthViewModel::class.java)
         binding.viewmodel = viewModel
         viewModel.authListner = this
+
+        val contentView: View = findViewById(R.id.layoutLogin)
+        contentView.setOnClickListener {
+            it.hideKeyboard()
+        }
     }
 
     override fun onStarted() {
@@ -34,11 +43,18 @@ class LoginActivity : AppCompatActivity(), AuthListner {
         progress_bar.hide()
         loginResponse.observe(this, Observer {
             toast(it)
+            Log.d("response", it)
         })
     }
 
     override fun onFailure(message: String) {
         progress_bar.hide()
         toast(message)
+    }
+
+    fun View.hideKeyboard() {
+        val inputMethodManager =
+            context!!.getSystemService(android.content.Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        inputMethodManager?.hideSoftInputFromWindow(this.windowToken, 0)
     }
 }
